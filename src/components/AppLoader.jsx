08f1141c3eb1715d595d7aa156a5de4729badc19
dipankarpaul2k <dipankarpaul2k@gmail.com>
2024-04-26 20:31:29 +0530

    minor changes in apploader

--- src/components/AppLoader.jsx
+++ src/components/AppLoader.jsx
@@ -2,8 +2,8 @@ import { Box, Center, Loader } from "@mantine/core";
 
 function AppLoader() {
   return (
-    <Box mih={"calc(100vh - 60px)"}>
-      <Center mih={"calc(100vh - 60px)"}>
+    <Box mih={"calc(90vh - 60px)"}>
+      <Center mih={"calc(90vh - 60px)"}>
         <Loader size={{ base: "md", xs: "lg" }} type="bars" />
       </Center>
     </Box>
