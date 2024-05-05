fd62583dce71a78c09b06c679396f76bc9958b88
dipankarpaul2k <dipankarpaul2k@gmail.com>
2024-05-05 21:17:57 +0530

    minor changes

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import { ModalsProvider } from "@mantine/modals";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <MantineProvider theme={theme} defaultColorScheme="light">
+    <MantineProvider theme={theme} defaultColorScheme="dark">
       <ModalsProvider>
         <App />
       </ModalsProvider>
